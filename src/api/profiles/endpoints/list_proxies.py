3c79d1273ab68db33e5f93c8faa60629f1e945fa
xenolex <alexey.belkov@gmail.com>
2025-10-02 15:12:33 +0400

    Add proxy list endpoint implementation

--- src/api/profiles/endpoints/list_proxies.py
+++ src/api/profiles/endpoints/list_proxies.py
@@ -1,3 +1,25 @@
-class ListProxiesEndpoint:
-    def list(self):
-        return NotImplemented
+from typing import Any, Dict
+
+from api.profiles._base import BaseEndpoint
+
+
+class ListProxiesEndpoint(BaseEndpoint):
+    """Endpoint for listing available proxy servers."""
+
+    def __init__(self, token: str) -> None:
+        super().__init__(token)
+        self._url = "https://api.controld.com/proxies"
+
+    def list(self) -> Dict[str, Any]:
+        """Returns list of usable proxies that traffic can be redirected through.
+
+        Returns:
+            Dict[str, Any]: Response data containing available proxy information.
+
+        Reference:
+            https://docs.controld.com/reference/get_proxies
+        """
+        # TODO: The response format is not documented, need to check it
+        response = self._session.get(self._url)
+        response.raise_for_status()
+        return response.json()
